b87973b4120661325a6551836c5dd094ecad2309
Kandili Dinesh <kandilindinesh@gmail.com>
2025-02-12 10:29:51 +0530

    Added Vercel Analytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import Navbar from "@/components/Navbar";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout({ children }) {
     return (
@@ -9,6 +10,7 @@ export default function RootLayout({ children }) {
                 <main className="min-h-screen flex flex-col justify-center">
                     {children}
                 </main>
+                <Analytics />
             </body>
         </html>
     );
